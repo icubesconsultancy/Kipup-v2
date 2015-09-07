54689f255b1e0bcd09c58d50b967bd60a8d073f0
snelpatel <snelpatel@gmail.com>
2015-09-07 11:31:01 +0530

    Screen Design Updated
    Updated to all screen designs.

---
+++ registration.h
@@ -0,0 +1,13 @@
+//
+//  registration.h
+//  Kip Up
+//
+//  Created by Jainam on 06/09/15.
+//  Copyright (c) 2015 jmsr. All rights reserved.
+//
+
+#ifndef Kip_Up_registration_h
+#define Kip_Up_registration_h
+
+
+#endif
